f3eb9a3d7d97b24b9059f80f873bf669671dc5ab
Yuzhong23 <xixixixifatur@gmail.com>
2024-04-23 11:29:45 +0700

    Membuat Publick di dalam class matakuliah

--- 0157_IntoOOP.cpp
+++ 0157_IntoOOP.cpp
@@ -19,6 +19,16 @@ private:
 	string kodeMk;
 	string namaMk;
 	int sks;
+public:
+	void input() {
+		cout << "Kode MK :";
+		cin >> kodeMk;
+		cout << "Nama MK :";
+		cin >> namaMk;
+		cout << "SKS :";
+		cin >> sks;
+	}
+
 
 };
 

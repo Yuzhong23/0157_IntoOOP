6dbf9698cc85dae7e8a0c904c0b32c09046f3710
Yuzhong23 <xixixixifatur@gmail.com>
2024-04-23 11:28:22 +0700

    Membuat void input di dalam class Mahasiswa

--- 0157_IntoOOP.cpp
+++ 0157_IntoOOP.cpp
@@ -7,7 +7,11 @@ public: // akses modifier
 	int umur;
 	string jurusan;
 
-
+	void output() {
+		cout << "Nama : " << nama << endl;
+		cout << "Umur : " << umur << endl;
+		cout << "Jurusan : " << jurusan << endl;
+	}
 };
 
 class Matakuliah {
